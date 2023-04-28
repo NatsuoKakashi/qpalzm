029369626ab63ed34f90433939a773f99a5da3dd
NatsuoKakashi <61655027+NatsuoKakashi@users.noreply.github.com>
2023-04-28 17:26:25 +0200

    Update setcharacter.js

--- Commands/Mods/setcharacter.js
+++ Commands/Mods/setcharacter.js
@@ -46,28 +46,9 @@ module.exports = {
 
 */
 
-      let botNames = ['Atlas MD', 'Power MD', 'Makima MD', 'Denji MD', 'Zero Two', 'Chika MD' , 'Miku MD', 'Marin MD','Ayanokoji MD','Ruka MD','Mizuhara MD','Rem MD','Sumi MD','Kaguya MD','Yumeko MD','Kurumi MD','Mai MD','Yor MD','Shinbou MD','Eiko MD']
+      let botNames = ['Jo Bot']
       let botLogos =[
-        'https://wallpapercave.com/wp/wp5924545.jpg',
-        'https://wallpapercave.com/wp/wp11253614.jpg',
-        'https://images5.alphacoders.com/126/1264439.jpg',
-        'https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/11/Chainsaw-Man-Denji-e-Power.webp?resize=1068%2C601&ssl=1',
-        'https://images3.alphacoders.com/949/949253.jpg',
-        'https://images4.alphacoders.com/100/1002134.png',
-        'https://wallpapercave.com/wp/wp10524580.jpg',
-        'https://images2.alphacoders.com/125/1257915.jpg',
-        'https://wallpapers.com/images/file/kiyotaka-ayanokoji-in-pink-qs33qgqm79ccsq7n.jpg',
-        'https://wallpapercave.com/wp/wp8228630.jpg',
-        'https://images3.alphacoders.com/128/1288059.png',
-        'https://images.alphacoders.com/711/711900.png',
-        'https://moewalls.com/wp-content/uploads/2022/07/sumi-sakurasawa-hmph-rent-a-girlfriend-thumb.jpg',
-        'https://wallpapercave.com/wp/wp6099650.png',
-        'https://wallpapercave.com/wp/wp5017991.jpg',
-        'https://wallpapercave.com/wp/wp2535489.jpg',
-        'https://images4.alphacoders.com/972/972790.jpg',
-        'https://images7.alphacoders.com/123/1236729.jpg',
-        'https://wallpapercave.com/wp/wp4650481.jpg',
-        'https://images8.alphacoders.com/122/1229829.jpg'
+        'https://i.imgur.com/VRwuTyl.jpg'
       ]
 
       await mkchar.findOne({id:'1'}).then(async (charInfo) => {
